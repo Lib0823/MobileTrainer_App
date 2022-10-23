b1ef114bd10c93dbbe1abc0f5b7fb8c0fe9f017a
Lib0823 <robot082373@naver.com>
2022-10-24 02:26:35 +0900

    10/24 Second Commit

--- app/src/main/java/com/example/project2/BoardActivity.java
+++ app/src/main/java/com/example/project2/BoardActivity.java
@@ -53,28 +53,23 @@ public class BoardActivity extends AppCompatActivity {
         cursor.moveToNext();   // 첫번째에서 다음 레코드가 없을때까지 읽음
         id = cursor.getString(0);
 
-        helperBoard.insertBoard(databaseBoard, "admin", "자유게시판입니다.", field);
-
         list = findViewById(R.id.list);
         List<String> data = new ArrayList<>();
         ArrayAdapter<String> adapter = new ArrayAdapter<>
                 (this, android.R.layout.simple_list_item_1, data);
         list.setAdapter(adapter);
+        list.setSelection(adapter.getCount() - 1);
 
         sql = "SELECT id, content FROM "+ helperBoard.tableNameBoard + " WHERE field = '"+ field +"'";
         cursor = databaseBoard.rawQuery(sql, null);
-        cursor.moveToNext();   // 첫번째에서 다음 레코드가 없을때까지 읽음
 
-        contentId = cursor.getString(0).toString();
-        content = cursor.getString(1).toString();
-
-        data.add(contentId+ " : " +content);
-        data.add("id : 안녕하세요~");
-        data.add("id2 : hihi");
-
+        while(cursor.moveToNext()){
+            contentId = cursor.getString(0);
+            content = cursor.getString(1);
+            data.add(contentId+ " : " +content);
+        }
         adapter.notifyDataSetChanged();
-
-
+        list.setSelection(adapter.getCount() - 1);
 
         // Field변경 시 저장
         boardNavi = findViewById(R.id.boardNavi);
@@ -85,15 +80,59 @@ public class BoardActivity extends AppCompatActivity {
                 {
                     case R.id.free:
                         field = "free";
+                        data.clear();
+                        sql = "SELECT id, content FROM "+ helperBoard.tableNameBoard + " WHERE field = '"+ field +"'";
+                        cursor = databaseBoard.rawQuery(sql, null);
+
+                        while(cursor.moveToNext()){
+                            contentId = cursor.getString(0);
+                            content = cursor.getString(1);
+                            data.add(contentId+ " : " +content);
+                        }
+                        adapter.notifyDataSetChanged();
+                        list.setSelection(adapter.getCount() - 1);
                         break;
                     case R.id.diet:
                         field = "diet";
+                        data.clear();
+                        sql = "SELECT id, content FROM "+ helperBoard.tableNameBoard + " WHERE field = '"+ field +"'";
+                        cursor = databaseBoard.rawQuery(sql, null);
+
+                        while(cursor.moveToNext()){
+                            contentId = cursor.getString(0);
+                            content = cursor.getString(1);
+                            data.add(contentId+ " : " +content);
+                        }
+                        adapter.notifyDataSetChanged();
+                        list.setSelection(adapter.getCount() - 1);
                         break;
                     case R.id.lean:
                         field = "lean";
+                        data.clear();
+                        sql = "SELECT id, content FROM "+ helperBoard.tableNameBoard + " WHERE field = '"+ field +"'";
+                        cursor = databaseBoard.rawQuery(sql, null);
+
+                        while(cursor.moveToNext()){
+                            contentId = cursor.getString(0);
+                            content = cursor.getString(1);
+                            data.add(contentId+ " : " +content);
+                        }
+                        adapter.notifyDataSetChanged();
+                        list.setSelection(adapter.getCount() - 1);
                         break;
                     case R.id.bulk:
                         field = "bulk";
+                        data.clear();
+                        sql = "SELECT id, content FROM "+ helperBoard.tableNameBoard + " WHERE field = '"+ field +"'";
+                        cursor = databaseBoard.rawQuery(sql, null);
+
+                        while(cursor.moveToNext()){
+                            contentId = cursor.getString(0);
+                            content = cursor.getString(1);
+                            data.add(contentId+ " : " +content);
+                        }
+                        adapter.notifyDataSetChanged();
+                        list.setSelection(adapter.getCount() - 1);
                         break;
                 }
                 return false;
@@ -101,6 +140,7 @@ public class BoardActivity extends AppCompatActivity {
         });
 
         contentText = findViewById(R.id.contentText);
+        contentText.requestFocus();
         //등록버튼 클릭 시
         contentBtn = findViewById(R.id.contentBtn);
         contentBtn.setOnClickListener(new View.OnClickListener() {
@@ -108,12 +148,24 @@ public class BoardActivity extends AppCompatActivity {
             public void onClick(View view) {
                 content = contentText.getText().toString();
                 contentText.setText("");
-
                 helperBoard.insertBoard(databaseBoard, id, content, field);
+                data.clear();
+
+                sql = "SELECT id, content FROM "+ helperBoard.tableNameBoard + " WHERE field = '"+ field +"'";
+                cursor = databaseBoard.rawQuery(sql, null);
+
+                while(cursor.moveToNext()){
+                    contentId = cursor.getString(0);
+                    content = cursor.getString(1);
+                    data.add(contentId+ " : " +content);
+                }
+                adapter.notifyDataSetChanged();
+                list.setSelection(adapter.getCount() - 1);
             }
         });
 
 
+        // 바텀 네비게이션
         bottomNavi = findViewById(R.id.bottonNavi);
         bottomNavi.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
